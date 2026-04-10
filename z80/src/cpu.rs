41b1dd8d319944fa52f083e560285ea039ff054c
Neil Herriford <neil.herriford@gmail.com>
2026-04-10 00:23:18 -0400

    Implement DI

--- z80/src/cpu.rs
+++ z80/src/cpu.rs
@@ -11,6 +11,7 @@ pub struct Cpu<B: Bus> {
 pub enum Instructions {
     NOP = 0,
     HALT = 0x76,
+    DI = 0xF3,
     #[num_enum(default)]
     Unknown,
 }
@@ -33,6 +34,12 @@ where
                 self.registers.increment_r();
                 self.registers.halted = true
             }
+            Instructions::DI => {
+                self.registers.increment_pc();
+                self.registers.increment_r();
+                self.registers.iff1 = false;
+                self.registers.iff2 = false;
+            }
             Instructions::Unknown => panic!("Unsupported instruction"),
         }
     }
@@ -50,8 +57,10 @@ mod tests {
         #[test]
         fn nop_advances_pc() {
             let mut cpu = Cpu::new(Registers::new(), TestBus::new());
+
             assert_eq!(cpu.registers.pc, 0);
             assert_eq!(cpu.registers.r, 0);
+
             cpu.step();
             assert_eq!(cpu.registers.pc, 1);
             assert_eq!(cpu.registers.r, 1);
@@ -60,14 +69,42 @@ mod tests {
         #[test]
         fn halt_stops() {
             let mut cpu = Cpu::new(Registers::new(), TestBus::new());
+
             cpu.bus.write8(0, Instructions::HALT.into());
             assert_eq!(cpu.registers.pc, 0);
             assert_eq!(cpu.registers.r, 0);
             assert!(!cpu.registers.halted);
+
             cpu.step();
             assert_eq!(cpu.registers.pc, 0);
             assert_eq!(cpu.registers.r, 1);
             assert!(cpu.registers.halted);
+        }
+
+        #[test]
+        fn di_disables_iff() {
+            let mut cpu = Cpu::new(Registers::new(), TestBus::new());
+
+            cpu.registers.iff1 = true;
+            cpu.registers.iff2 = true;
+            cpu.bus.write8(0, Instructions::DI.into());
+            cpu.bus.write8(1, Instructions::DI.into());
+            assert_eq!(cpu.registers.pc, 0);
+            assert_eq!(cpu.registers.r, 0);
+            assert!(cpu.registers.iff1);
+            assert!(cpu.registers.iff2);
+
+            cpu.step();
+            assert_eq!(cpu.registers.pc, 1);
+            assert_eq!(cpu.registers.r, 1);
+            assert!(!cpu.registers.iff1);
+            assert!(!cpu.registers.iff2);
+
+            cpu.step();
+            assert_eq!(cpu.registers.pc, 2);
+            assert_eq!(cpu.registers.r, 2);
+            assert!(!cpu.registers.iff1);
+            assert!(!cpu.registers.iff2);
         }
     }
 }
